764bee1d60dc0d0c39bb9b3121d814bbdd9912c2
Zealot-Z <haotian2@andrew.cmu.edu>
2018-11-13 21:09:15 -0500

    Chapter1

---
+++ Chapter1/Q1.java
@@ -0,0 +1,15 @@
+/* 
+    Is Unique: Implement an algorithm to determine if a string has all unique characters.
+    What if you cannot use additional data structures? 
+*/
+
+public class Q1{
+    public static void main (String[] arg)
+    {
+        
+    }
+
+    public boolean isUnique(){
+
+    }
+}
